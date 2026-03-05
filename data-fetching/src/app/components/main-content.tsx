b7cea1caf467deed30c24fe7f6bb1652b9d41a4f
vickbk <vickbake8@gmail.com>
2026-03-05 10:34:55 +0200

    initializing direct data fetching

---
+++ data-fetching/src/app/components/main-content.tsx
@@ -0,0 +1,10 @@
+import { EffectFetching } from "@/features/direct-fetching/components/effect-fetching";
+
+export const MainContent = () => {
+  return (
+    <main>
+      <h1>Data fetching techniques</h1>
+      <EffectFetching />
+    </main>
+  );
+};
---
+++ data-fetching/src/features/direct-fetching/components/effect-fetching.tsx
@@ -0,0 +1,9 @@
+import { CategoryHolder } from "@/shared/common-layout/components/category-holder";
+
+export const EffectFetching = () => {
+  return (
+    <CategoryHolder title="Direct fetching with effect">
+      fetching data will go here
+    </CategoryHolder>
+  );
+};
