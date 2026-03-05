6efc004b37a6870c4085fa44c1c2e7274d9dd9f1
vickbk <vickbake8@gmail.com>
2026-03-05 12:48:46 +0200

    feat: adding react query hook

--- data-fetching/src/app/components/main-content.tsx
+++ data-fetching/src/app/components/main-content.tsx
@@ -1,9 +1,11 @@
 import { EffectFetching } from "@/features/direct-fetching/components/effect-fetching";
+import { ReactQueryFetching } from "@/features/react-query/components/react-query-fetching";
 
 export const MainContent = () => {
   return (
     <main>
       <h1>Data fetching techniques</h1>
+      <ReactQueryFetching />
       <EffectFetching />
     </main>
   );
---
+++ data-fetching/src/features/react-query/components/react-query-fetching.tsx
@@ -0,0 +1,9 @@
+import { CategoryHolder } from "@/shared/common-layout/components/category-holder";
+
+export const ReactQueryFetching = () => {
+  return (
+    <CategoryHolder title="Data fetching with React Query">
+      will go here
+    </CategoryHolder>
+  );
+};
